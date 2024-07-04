9f8296c6b542ebe0ca5fac845607a46e51b565c6
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-04 22:41:47 +0200

    [UPD] created IF structure with its IF and ELSE statements

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -1,2 +1,17 @@
 const evenOrOdd = prompt ('Scegli pari o dispari')
 console.log("hai scelto: " , evenOrOdd)
+
+if ((evenOrOdd.toLowerCase() === "pari") || (evenOrOdd.toLowerCase() === "dispari")) {
+    const number = Number(prompt ('Inserisci un numero tra 1 e 5'));
+    if (number >= 1 && number <= 5) {
+        let result = sumAndResult(Math.floor(Math.random() * 5 + 1), number)
+        console.log("La somma dei due numeri è: " , result)
+        if(result === evenOrOdd) {
+            console.log("hai vinto tu!!")
+         } else {
+            console.log("hai perso!!")
+         }
+    } else {
+        console.log('Devi inserire un numero tra 1 e 5')
+    }
+}
